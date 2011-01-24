6fdff22b2bf3796f0c29db9a52608d9028b7849d
H.Merijn Brand <merijn@tux09.procura.nl>
2011-01-24 17:34:07 +0100

    Add Changelog

--- Gnokii.pm
+++ Gnokii.pm
@@ -14,7 +14,7 @@ our @ISA = qw(Exporter DynaLoader);
 our %EXPORT_TAGS = ( all => [ qw( ) ] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{all} } );
 our @EXPORT      = qw( );
-our $VERSION     = "0.02";
+our $VERSION     = "0.03";
 
 my @MEMORYTYPES = qw(
     ME SM FD ON EN DC RC MC LD MT TA CB IN OU AR TE
