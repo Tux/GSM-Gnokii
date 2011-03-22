532371e7b242b22c8da9d3a4cd30301ee7624a30
H.Merijn Brand <merijn@tux09.procura.nl>
2011-03-22 16:00:40 +0100

    Yeah, GSMtree works as I want

--- examples/GSMTree.pm
+++ examples/GSMTree.pm
@@ -33,6 +33,7 @@ sub Populate
 {
     my ($w, $args) = @_;
 
+    # print STDERR "Populate ...\n";
     my $data = $w->privateData;
     %$data = %def_conf;
 
@@ -66,6 +67,7 @@ sub Populate
 sub configure
 {
     my ($w, %cnf) = @_;
+    # print STDERR "configure ...\n";
     delete @cnf{map { ( $_, "-$_" ) } keys %{$w->privateData}};
     $w->SUPER::configure (%cnf);
     } # configure
@@ -89,22 +91,31 @@ sub DirCmd
     my $pd = $w->privateData;
     my $mt = ($dir =~ s{^([AB]):}{}) ? $mt{$1} : $pd->{memtype} // "ME";
     $dir = _cleanpath ($dir);
-    print STDERR "DirCmd ($mt, $dir, 1)\n";
+    # print STDERR "DirCmd ($mt, $dir, 1)\n";
     my $dt;
     if ($dt{$mt}{$dir}) {
-	print STDERR "Get from cache\n";
+	# print STDERR "Get from cache\n";
 	$dt = $dt{$mt}{$dir};
 	}
     else {
 	(my $gdir = $dir) =~ s{/+$}{};
 	$gdir =~ s{/}{\\}g;
 	$dt = $pd->{gsm}->GetDir ($mt, $gdir, 1) or return;
-	ref $dt eq "HASH" && exists $dt->{tree}  or return;
+	ref $dt eq "HASH"                        or return;
+	# Clean up
+	if ($dt->{tree} && ref $dt->{tree} eq "ARRAY") {
+	    my @tree = grep { $_->{id} } @{$dt->{tree}};
+	    if (@tree) {
+		$dt->{tree} = \@tree;
+		}
+	    else {
+		delete $dt->{tree};
+		}
+	    }
 	$dt{$mt}{$dir} = $dt;	# Cache!
 	}
-    my @names = grep { $_ ne "." && $_ ne ".." } map {
-	$dt->{tree}[$_]{name} } 0..($dt->{file_count} - 1) or return;
-    print STDERR "Names: (@names)\n";
+    my @names = map { $_->{name} } @{$dt{$mt}{$dir}{tree}//[]} or return;
+    # print STDERR "Names: (@names)\n";
     $showhidden or @names = grep !m/^[.]/ => @names;
     return @names;
     } # DirCmd
@@ -113,7 +124,7 @@ sub DirCmd
 sub directory
 {
     my ($w, $key, $val) = @_;
-    print STDERR "Directory ...\n";
+    # print STDERR "directory ...\n";
     # We need a value for -image, so its being undefined is probably caused
     # by order of handling config defaults so defer it.
     #$w->afterIdle ([$w, "set_dir" => $val]);
@@ -122,7 +133,7 @@ sub directory
 sub set_dir
 {
     my ($w, $val) = @_;
-    print STDERR "set_dir ($val)\n";
+    # print STDERR "set_dir ($val)\n";
     my $fulldir = _cleanpath ($val);
 
 
@@ -146,12 +157,12 @@ sub OpenCmd
 {
     my ($w, $dir) = @_;
 
-print STDERR "OpenCmd ($dir)\n";
+    # print STDERR "OpenCmd ($dir)\n";
     my $parent = $dir;
     foreach my $name ($w->dirnames ($parent)) {
 	$name eq "." || $name eq ".." || $name eq "/" and next;
 	my $subdir = _cleanpath ("$dir/$name");
-print STDERR "> OpenCmd ($subdir)\n";
+	# print STDERR "> OpenCmd ($subdir)\n";
 	# $dt{$mt}{$dir} or $w->DirCmd ($subdir, 0);
 	if ($w->infoExists ($subdir)) {
 	    $w->show (-entry => $subdir);
@@ -167,8 +178,7 @@ sub add_to_tree
 {
     my ($w, $dir, $name, $parent) = @_;
 
-    #$parent //= "/";
-printf STDERR "Add (%-20s  %-20s %s\n", $dir, $name, $parent // "--undef--";
+    # printf STDERR "Add (%-20s  %-20s %s\n", $dir, $name, $parent // "--undef--";
     my $image;# = $w->cget ("-image");
 #   UNIVERSAL::isa ($image, "Tk::Image") or
 #	$image = $w->Getimage ($image);
@@ -195,24 +205,16 @@ sub has_subdir
     my ($w, $dir) = @_;
     my $mt = $w->privateData->{memtype};
     $dir = _cleanpath ($dir);
-    print STDERR "Has subdir ($mt, $dir) ?\n";
-    my @n = $w->DirCmd ($dir, 1) or return 0;
-    print STDERR "Hah! 1\n";
-    my $t = $dt{$mt}{$dir}{tree} or return 0;
-    print STDERR "Hah! 2\n";
-    for (@$t) {
-	# DDumper $_;
-	$_->{file_count} // 0 and return 1;
-	$_->{tree} && ref $_->{tree} && @{$_->{tree}} and return 1;
-	}
-    print STDERR "Hah! 3\n";
+    # print STDERR "#"x78,"\n","Has subdir ($mt, $dir) ?\n";
+    exists $dt{$mt}{$dir} or $w->DirCmd ($dir, 1);
+    exists $dt{$mt}{$dir}{tree} and return 1;
     return 0;
     } # has_subdir
 
 sub dirnames
 {
     my ($w, $dir) = @_;
-    print STDERR "dirnames ($dir) ...\n";
+    # print STDERR "dirnames ($dir) ...\n";
     my @names = $w->Callback ("-dircmd", $dir, $w->cget ("-showhidden"));
     return (@names);
     } # dirnames
@@ -221,78 +223,5 @@ sub show_cache
 {
     DDumper { Cache => \%dt };
     } # show_cache
-
-{
-
-package Tk::DirTreeDialog;
-use base qw(Tk::Toplevel);
-Construct Tk::Widget "DirTreeDialog";
-
-sub Populate
-{
-    my ($w, $args) = @_;
-    $w->{curr_dir} = delete $args->{-initialdir};
-    $w->{curr_dir} //= "/";
-    defined $args->{-mustexist} and
-	die "-mustexist is not yet implemented";
-
-    my $title = $args->{-title} || "Choose directory:";
-    delete $args->{-popover};
-
-    $w->title ($title);
-    $w->{ok} = 0;    # flag: "1" means OK, "-1" means cancelled
-
-    $w->transient ($w->Parent->toplevel);
-
-    # Create Frame widget before the DirTree widget, so it's always visible
-    # if the window gets resized.
-    my $f = $w->Frame->pack (-fill => "x", -side => "bottom");
-
-    my $d;
-    $d = $w->Scrolled (
-	"DirTree",
-	-scrollbars      => "osoe",
-	-width           => 35,
-	-height          => 20,
-	-selectmode      => "browse",
-	-exportselection => 1,
-	-browsecmd       => sub { $w->{curr_dir} = shift; },
-
-	# With this version of -command a double-click will
-	# select the directory
-	-command         => sub { $w->{ok} = 1 },
-
-	# With this version of -command a double-click will
-	# open a directory. Selection is only possible with
-	# the Ok button.
-	#-command        => sub { $d->opencmd($_[0]) },
-	)->pack (-fill => "both", -expand => 1);
-    # Set the initial directory
-    $d->set_dir ("/");
-
-    $f->Button (
-	-text    => "Ok",
-	-command => sub { $w->{ok} =  1 })->pack (-side => "left");
-    $f->Button (
-	-text    => "Cancel",
-	-command => sub { $w->{ok} = -1 })->pack (-side => "left");
-    $w->OnDestroy (sub { $w->{ok} = -1 });
-    } # Populate
-
-sub Show
-{
-    my $w         = shift;
-    my $old_focus = $w->focusSave;
-    my $old_grab  = $w->grabSave;
-    Tk::catch { $w->grab; };
-    $w->waitVariable (\$w->{ok});
-    my $ret = $w->{ok} == 1 ? $w->{curr_dir} : undef;
-    Tk::Exists ($w) and $w->grabRelease;
-    &$old_focus;
-    &$old_grab;
-    Tk::Exists ($w) and $w->destroy;
-    $ret;
-    } # Show
-}
 
 1;
