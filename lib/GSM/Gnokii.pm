617e0e647778da414ed12e17425173388fedd3f6
H.Merijn Brand <merijn@lx09.procura.nl>
2011-09-07 18:14:23 +0200

    NAME / DISTNAME in Makefile.PL

--- lib/GSM/Gnokii.pm
+++ lib/GSM/Gnokii.pm
@@ -14,7 +14,7 @@ our @ISA = qw(Exporter DynaLoader);
 our %EXPORT_TAGS = ( all => [ qw( ) ] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{all} } );
 our @EXPORT      = qw( );
-our $VERSION     = "0.06";
+our $VERSION     = "0.07";
 
 bootstrap GSM::Gnokii $VERSION;
 
